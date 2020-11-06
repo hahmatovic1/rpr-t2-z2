b55085e701555aced8fb40ae614d803d7bd54ac5
Hadija Ahmatovic <hahmatovic1@etf.unsa.ba>
2020-11-06 19:21:32 +0100

    inicijalni komit. kreirani konstruktori i metode isNull i isIn

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,35 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    double pocetnaTacka, krajnjaTacka;
+    boolean pocetnaPripada;
+    boolean krajnjaPripada;
 
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pocetnaPripada, boolean krajnjaPripada) {
+        if(pocetnaTacka>krajnjaTacka) throw new IllegalAccessException("pocetna tacka veca od krajnje");
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pocetnaPripada = true;
+        this.krajnjaPripada = true;
+    }
+
+    public Interval(){
+        pocetnaTacka = 0;
+        krajnjaTacka = 0;
+        pocetnaPripada = false;
+        krajnjaPripada = false;
+    }
+
+    public boolean isNull(){
+        if(pocetnaTacka == krajnjaTacka)
+            return true;
+        return false;
+    }
+
+    public boolean isIn(double tacka){
+        if(pocetnaTacka<tacka && tacka<krajnjaTacka)
+            return true;
+        return false;
+    }
 
 }
